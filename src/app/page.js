de6e7120ac9bd1d377a2bbf097b64beeb409bd13
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-21 20:49:38 +0600

    remove lorem ispum

--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,22 @@ import RightCard from "@/components/RightCard"
 
 
 const Home = async() => {
-  // const { data: allCategories } = await getAllNews();
-  // console.log(allCategories);
+
   return (
     <div>
-
-     
-
-      <div className="grid grid-cols-1 lg:grid-cols-7 gap-7">
-        <div className="col-span-4">
-        <div className="py-4">
-        <span className="text-3xl lg:text-5xl font-semibold bg-gray-200 px-4 py-3 rounded-sm">Disscussion Fourm</span>
+<div className="flex lg:justify-around">
+  
+<div className="py-4">
+        <span className="text-xl bg-[#122033] lg:bg-gray-200 text-white lg:text-rose-500  lg:text-5xl font-semibold px-4 py-3 rounded-sm">Disscussion Fourm</span>
       </div>
+      <div className="py-4">
+        <span className="text-xl bg-[#122033] lg:bg-gray-200 text-white lg:text-rose-500 lg:text-5xl font-semibold  px-4 py-3 rounded-sm">Market Stories</span>
+      </div>
+</div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-7 gap-3 lg:gap-7">
+        <div className="col-span-4">
+       
          <div className="flex flex-col mt-4">
          <Card/>
           <Card/>
@@ -24,11 +28,11 @@ const Home = async() => {
           <Card/>
          </div>
           </div>
-        <div className="col-span-3">
-        <div className="py-4">
-        <span className="text-3xl lg:text-5xl font-semibold bg-gray-200 px-4 py-3 rounded-sm">Market Stories</span>
-      </div>
+        <div className="col-span-3 flex-1">
+       
       <div className="mt-8">
+      <RightCard/>
+      <RightCard/>
       <RightCard/>
       </div>
         </div>
