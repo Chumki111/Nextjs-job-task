4cb5283bfa6b5ff26a1136d7fac35b1c0d163fd8
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-21 21:13:18 +0600

    sidebar menu add

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const MenuItem = ({item,route,icon:Icon}) => {
   return (
-    <div>
+    <div className="py-3 hover:bg-[#122033] hover:px-7">
         <Link href={route} className="flex items-start gap-2">
                 <Icon className=" text-2xl"/>
                <span className="font-semibold">{item}</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,16 +6,8 @@ import { IoIosNotifications } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
 import MenuItem from './MenuItem';
 import { GoCommentDiscussion } from "react-icons/go";
-const menuItems = [
-  {
-    route: 'Discussion Forum',
-    pathname: '/'
-  },
-  {
-    route: 'gf',
-    pathname: 'sd'
-  }
-]
+import { TbBrandStorybook } from "react-icons/tb";
+
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleSidebar = () => {
@@ -31,7 +23,7 @@ const Sidebar = ({ children }) => {
       <div className=''>
         {/* sidebar */}
         <div
-          className={`fixed inset-y-0 h-full md:sticky left-0 z-50 bg-[#122033] text-white  md:px-0 md:py-0 transition-all duration-300 ${isOpen ? 'visible left-0 w-[250px] opacity-100 md:w-[310px]' : 'invisible -left-4 opacity-0 md:w-0 md:overflow-hidden md:opacity-100'}`}
+          className={`fixed inset-y-0 h-full md:sticky left-0 z-50 bg-blue-950 text-white  md:px-0 md:py-0 transition-all duration-300 ${isOpen ? 'visible left-0 w-[250px] opacity-100 md:w-[310px]' : 'invisible -left-4 opacity-0 md:w-0 md:overflow-hidden md:opacity-100'}`}
         >
           {/* sidebar header */}
           <div className='py-5 shadow-md flex justify-between items-start px-7'>
@@ -44,28 +36,53 @@ const Sidebar = ({ children }) => {
           <hr />
           {/* sidebar menu */}
           <div className='pt-4 px-7'>
-            
-           
-           <MenuItem item="Disscussion Fourm" route="/" icon={GoCommentDiscussion}/>
+
+
+            <MenuItem item="Disscussion Fourm" route="/" icon={GoCommentDiscussion} />
+            <MenuItem item="Market Stories" route="" icon={TbBrandStorybook} />
+            <div className="py-3 hover:bg-[#122033] hover:px-7">
+              <span className="font-semibold pl-8">Sentiment</span>
+
+            </div>
+            <div className="py-3 hover:bg-[#122033] hover:px-7">
+              <span className="font-semibold pl-8">Market</span>
+
+            </div>
+            <div className="py-3 hover:bg-[#122033] hover:px-7">
+              <span className="font-semibold pl-8">Sector</span>
+
+            </div>
+            <div className="py-3 hover:bg-[#122033] hover:px-7">
+              <span className="font-semibold pl-8">Watchlist</span>
+
+            </div>
+            <div className="py-3 hover:bg-[#122033] hover:px-7">
+              <span className="font-semibold pl-8">Events</span>
+
+            </div>
+            <div className="py-3 hover:bg-[#122033] hover:px-7">
+              <span className="font-semibold pl-8">News/Interview</span>
+
+            </div>
           </div>
         </div>
 
       </div>
       <div className="size-full min-h-screen">
-       <div className='bg-gray-400'>
-         {/* sidebar open and close button */}
-         {isOpen ? (
-          <button className="fixed top-1/2 left-[260px] py-5 lg:left-[320px] transform -translate-y-1/2 bg-[#122033] text-white" onClick={closeSidebar}>
-            <IoMdArrowDropright size={24} />
-          </button>
-        ) : (
-          <div>
-            <button className="fixed bg-[#122033] py-5 top-1/2 left-4 transform -translate-y-1/2 text-[#fff]" onClick={toggleSidebar}>
-            <IoMdArrowDropright size={24} />
-          </button>
+        <div className='bg-gray-400'>
+          {/* sidebar open and close button */}
+          {isOpen ? (
+            <button className="fixed top-1/2 left-[260px] py-5 lg:left-[320px] transform -translate-y-1/2 bg-[#122033] text-white" onClick={closeSidebar}>
+              <IoMdArrowDropright size={24} />
+            </button>
+          ) : (
+            <div>
+              <button className="fixed bg-[#122033] py-5 top-1/2 left-4 transform -translate-y-1/2 text-[#fff]" onClick={toggleSidebar}>
+                <IoMdArrowDropright size={24} />
+              </button>
             </div>
-        )}
-       </div>
+          )}
+        </div>
 
         {/* outlet - part  */}
         <div className=" p-2 md:px-10">{children}</div>
